575be3847e93603dd488c09f777f77bad1092403
tking8 <54691809+tking8@users.noreply.github.com>
2020-09-27 23:05:03 -0700

    Add files via upload

---
+++ point.py
@@ -0,0 +1,31 @@
+class point:
+    def __init__(self, dim, data):
+        self.dim=dim
+        self.data=[]
+        for i in range(dim):
+            self.data.append(float(data[i]))
+    def __repr__(self):
+        output=""
+        for i in self.data:
+            output+=str(i)+" "
+        return output
+    def scale(self, x):
+        for i in range(self.dim):
+            self.data[i]*=x
+    def dist(self, p2):
+        import math
+        sum=0
+        for i in range(self.dim):
+            sum+=math.sqrt((self.data[0]-p2.data[0])**2+\
+                                  (self.data[1]-p2.data[1])**2+\
+                                  (self.data[2]-p2.data[2])**2)
+        return sum
+    def mirror(self):
+        for i in range(self.dim):
+            self.data[i]=-self.data[i]
+    def dot(self, apoint):
+        sum=0
+        for i in range(self.dim):
+            sum+=self.data[i]*apoint.data[i]
+        return sum
+
